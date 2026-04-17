87d4ecd452a18e5394f9c55439a77576a5abe76b
najongjine <najongjin3@hotmail.com>
2026-04-17 20:27:53 +0900

    .

---
+++ python/p260417.py
@@ -0,0 +1,12 @@
+a=1
+b=2.1
+str1="ss"
+b1=True
+set1={'A', 'B', 'C'}
+tuple1=(1,2,3)
+dict1={'name':'John', 'age':30, 'city':'New York'}
+print(f"a의 타입 {type(a)}")
+print(f"b의 타입 {type(b)}")
+print(f"str1의 타입 {type(str1)}")
+print(f"b1의 타입 {type(b1)}")
+print(f"set1의 타입 {type(set1)}")
