6bd239146c52dc33d9ad8e21b67f6b8be2c3e660
najongjine <najongjin3@hotmail.com>
2026-04-17 20:43:08 +0900

    .

--- python/p260417.py
+++ python/p260417.py
@@ -1,12 +1,14 @@
-a=1
+a=(1)
 b=2.1
 str1="ss"
+str2="ss"
 b1=True
-set1={'A', 'B', 'C'}
-tuple1=(1,2,3)
+set1={'A', 'B', 'C','C','C'} # 세트는 중복이 없다
+set_list = list(set(set1)) # 세트를 리스트로 변환
+tuple1=(1,2) # 한번 만들면, 원소값을 바꿀수 없다
+tuple1=(2,3,54,65,5,3,3,4,4)
 dict1={'name':'John', 'age':30, 'city':'New York'}
-print(f"a의 타입 {type(a)}")
-print(f"b의 타입 {type(b)}")
-print(f"str1의 타입 {type(str1)}")
-print(f"b1의 타입 {type(b1)}")
-print(f"set1의 타입 {type(set1)}")
+
+arr1=[1,1,2,3,4,5]
+arr1_set = set(arr1) # 리스트를 세트로 변환
+print(f"{arr1_set}")
