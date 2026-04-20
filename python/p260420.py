de51de2f2e87a167b184e5cf218531663ae10198
najongjine <najongjin3@hotmail.com>
2026-04-20 19:15:02 +0900

    .

--- python/p260420.py
+++ python/p260420.py
@@ -3,4 +3,13 @@ set1={1,2,2,3}
 # dicnationary 는 C 언의 struct이랑 비슷 하다.
 dic1={'name':'John', 'age':30, 'city':'New York'}
 
-print(dic1['city'])
+####################
+
+numbers = [1, 2, 3, 4, 5]
+
+result = {}
+
+for num in numbers:
+    result[num] = num * num
+
+print(result)
