aa904c50a0aaaf5415d5dca69b719ef80f25ea9b
najongjine <najongjin3@hotmail.com>
2026-04-21 20:00:59 +0900

    .

--- python/p260421_2.py
+++ python/p260421_2.py
@@ -1,9 +1,11 @@
 #########클래스############
 class Person:
-    def __init__(self):
-        self.a=1
-        self.name="페페"
+    def __init__(self,a,b):
+        self.a=a
+        self.name=b
         pass
 
 # 객체 생성
-p1 = Person()
+p1 = Person(0,"모모")
+p2=Person(1,"페페")
+
