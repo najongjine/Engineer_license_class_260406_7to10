f04663166f43bf5d31555abb65ce3a63d1202c22
najongjine <najongjin3@hotmail.com>
2026-04-21 19:16:20 +0900

    .

--- python/p260421.py
+++ python/p260421.py
@@ -8,24 +8,24 @@ if(1){
 # 파이썬은 함수, class 영역 빼고는 scope가 없습니다
 a=0
 if a:
-    print("true 로 판정")
+    #print("true 로 판정")
     a+=1
 elif a<0:
-    print("a가 0보다 작다 판정")
+    #print("a가 0보다 작다 판정")
     --a
 else:
-    print("0 으로 판정")
+    #print("0 으로 판정")
     a=0
 
 for i in range(3):
     if(i%2==0):
         break
     i=i
-    print(i)
+    #print(i)
 
 for i in [1,2]:
     i=i
-    print(i)
+    #print(i)
 
 #########함수###########
 def add(a,b):
