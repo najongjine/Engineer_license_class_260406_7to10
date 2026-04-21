5823faeaeac3f030b02034e3e260c83858274dab
najongjine <najongjin3@hotmail.com>
2026-04-21 20:14:44 +0900

    .

--- python/p260421_2.py
+++ python/p260421_2.py
@@ -1,5 +1,6 @@
 #########클래스############
 class Person:
+    c="클래스 변수"
     def __init__(self,a,b):
         self.a=a
         self.name=b
@@ -8,4 +9,46 @@ class Person:
 # 객체 생성
 p1 = Person(0,"모모")
 p2=Person(1,"페페")
+print(f"p1" ,p1.c)
+print(f"p2" ,p2.c)
+p1.c="클래스 변수2"
+print(f"p1" ,p1.c)
+print(f"p2" ,p2.c)
 
+Person.c="클래스 변수3"
+print(f"Person.c=클래스 변수3" )
+print(f"p1" ,p1.c)
+print(f"p2" ,p2.c)
+
+"""
+파이썬의 클래스 변수는 자바의 static 속성과 많이 틀려요.
+인스턴스가 생성됬을땐 class 의 클래스변수와 연결이 되있습니다.
+하지만 인스턴스.클래스변수 = "값바꿈" 이렇게 하면 
+인스턴스에 새로운 클래스 변수가 생성되서 class.클래스 변수와의 연결이 끊어져요.
+만약 인스턴스의 클래스변수 값을 바꾸지 않았다면, class.클래스변수와 연결이 되있습니다.
+클래스이름.클래스변수="값바꿈" 하면 연결되 있는 인스턴스들의 클래스변수는 값이 바뀌어요.
+"""
+
+class Parent:
+    def __init__(self):
+        self.value="부모"
+        pass
+    def show(self):
+        print(f"난 부모show",self.value)
+
+class Child(Parent):
+    def __init__(self):
+        super().__init__()
+        pass
+    def show(self):
+        print(f"난 자식show",self.value)
+
+child=Child()
+child.value="자식"
+print(child.value)
+child.show()
+
+p=Parent()
+p.value="부모2"
+print(p.value)
+p.show()
