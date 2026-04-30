ccee7fe9c969ca044d6c452e1af848a6e44c4868
najongjine <najongjin3@hotmail.com>
2026-04-30 21:50:19 +0900

    .

--- python/test.py
+++ python/test.py
@@ -1,18 +1,12 @@
-lst = [1,2,3] # [99,2,3]
-"""
-dst={
-1:2
-2:7
-3:6
-}
-"""
-dst = {i : i* 2 for i in lst}
-# s={2,4,6,99}
-s = set(dst.values())
-lst[0] = 99 
-dst[2]=7
-s.add(99)
-"""
-{2,4,6,99} & {2,4,7} -> len({2,4}) = 2
-"""
-print(len(s & set(dst.values())))
+def func(lst):
+    #               6//2 == 3 -> 0,1,2
+    for i in range(len(lst) //2):
+        """
+        lst[1], lst[2] = lst[2], lst[1]
+        """
+        lst[i], lst[-i-1] = lst[-i-1], lst[i]
+# [6,5,4,3,2,1] 
+lst = [1,2,3,4,5,6] 
+func(lst)
+#          12      -        9
+print(sum(lst[::2]) - sum(lst[1::2]))
